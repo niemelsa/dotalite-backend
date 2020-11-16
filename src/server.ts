82f29065dfe8c034e2662a1c193e04d3c4ae43e3
Sami Niemelä <samppuu@outlook.com>
2020-11-16 17:41:34 +0200

    added prisma

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,18 @@ import express, { Application } from "express";
 import config from './config';
 import initMiddleware from './startup/middleware';
 import initRoutes from './startup/routes';
+import initAuth from './startup/auth';
 
-(async () => {
-
+async function bootstrap() {
     const app: Application = express();
 
     await initMiddleware(app);
     await initRoutes(app);
+    await initAuth(app);
 
     app.listen(config.port, () => {
         console.log(`Listening on port ${config.port}...`);
     })
+};
 
-})();
+bootstrap();
