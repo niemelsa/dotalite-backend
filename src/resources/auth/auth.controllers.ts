b2926fa11cac4d3795203bb3fdc1892a9f6af100
Sami Niemelä <samppuu@outlook.com>
2020-11-18 15:59:40 +0200

    minor changes in the auth controller

--- src/resources/auth/auth.controllers.ts
+++ src/resources/auth/auth.controllers.ts
@@ -1,3 +1,4 @@
+import { newToken } from './../../utils/jwt';
 import { UserRequest } from './../../interfaces/user-request.interface';
 import { PrismaClient } from '@prisma/client';
 import { Response } from 'express';
@@ -5,7 +6,13 @@ import { Response } from 'express';
 const prisma = new PrismaClient();
 
 const handleLogin = async (req: UserRequest, res: Response) => {
-  console.log(req.user);
+  try {
+    const token = newToken(req.user);
+    res.status(200).header('token', token).redirect('/');
+  } catch (e) {
+    console.error(e);
+    res.send({ message: 'shit happens' });
+  }
 };
 
 export default {
